7a0de877dda87d1cc5d51e536943552a801629a1
Anag-bey Kwame <kwameicon.bs.gh@gmail.com>
2023-12-26 10:07:02 +0000

    React Hexadecimal Background Color Change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [backgroundColor, setBackgroundColor] = useState("#ffffff");
+
+  useEffect(() => {
+    function generateRandomHexadecimalColorCode() {
+      const letters = "0123456789ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+
+    function changeBackgroundColor() {
+      const randomColor = generateRandomHexadecimalColorCode();
+      document.body.style.backgroundColor = randomColor;
+      setBackgroundColor(randomColor);
+    }
+
+
+    // Attach event listener to button
+    const button = document.querySelector(".changeBackground");
+    button.addEventListener("click", changeBackgroundColor);
+
+    // Cleanup function
+    return () => {
+      button.removeEventListener("click", changeBackgroundColor);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <h1 className="header">Click to change the background Color</h1>
+      <div className="hexOutputField">{backgroundColor}</div>
+      <section className="action">
+        <div className="btn changeBackground">Change background</div>
+      </section>
+    </main>
   );
 }
 
